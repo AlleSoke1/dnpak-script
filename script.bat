80c163ebaa51e679b42c10b8841ffe3c6ed49b6d
alin1337 <alin1337@users.noreply.github.com>
2014-12-28 19:26:32 +0200

    Create script.bat

---
+++ script.bat
@@ -0,0 +1,49 @@
+@echo off
+set mode=0
+set outputdir=extracted
+color A
+echo --------------------------------------------
+echo --------------------------------------------
+echo DragonNest .pak extractor script by Alin1337
+echo --------------------------------------------
+echo --------------------------------------------
+pause
+echo 0 = Extract only DNT
+echo 1 = Extract only *.LUA\*.XML
+echo 2 = Extract all client resources\mapdata w/o:avi,ogg,dds,mp3,wav
+echo 3 = Extract all client resource\mapdata
+SET/P mode="Enter extraction mode [default: %mode%]:"
+SET/P outputdir=Enter directory name [default: %outputdir%]:
+
+if /i %mode%==0 goto only_dnt
+if /i %mode%==1 goto only_script
+if /i %mode%==2 goto all_noaudiovideo
+if /i %mode%==3 goto all
+
+
+
+:only_dnt
+FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "*.dnt" quickbms.txt Resource0%%A.pak %outputdir%
+quickbms.exe -Y -f "*.dnt" quickbms.txt Resource10.pak %outputdir%
+quickbms.exe -Y -f "*.dnt" quickbms.txt Resource11.pak %outputdir%
+
+:only_script
+FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource0%%A.pak %outputdir%
+quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource10.pak %outputdir%
+quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource11.pak %outputdir%
+
+:all_noaudiovideo
+FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource0%%A.pak %outputdir%
+quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource10.pak %outputdir%
+quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource11.pak %outputdir%
+
+:all
+FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y quickbms.txt Resource0%%A.pak %outputdir%
+quickbms.exe -Y quickbms.txt Resource10.pak %outputdir%
+quickbms.exe -Y quickbms.txt Resource11.pak %outputdir%
+
+
+echo .
+echo .
+echo "DONE! Extracted files to %outputdir%."
+pause
