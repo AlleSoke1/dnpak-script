7fcc9166c641623634a6174d402109ebbcc30f25
alin1337 <alin1337@users.noreply.github.com>
2015-05-18 05:26:11 +0300

    improvement? ^^

--- script.bat
+++ script.bat
@@ -24,30 +24,22 @@ if /i %mode%==3 goto all
 
 :only_dnt
 FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "*.dnt" quickbms.txt Resource0%%A.pak %outputdir%
-quickbms.exe -Y -f "*.dnt" quickbms.txt Resource10.pak %outputdir%
-quickbms.exe -Y -f "*.dnt" quickbms.txt Resource11.pak %outputdir%
-quickbms.exe -Y -f "*.dnt" quickbms.txt Resource12.pak %outputdir%
+FOR %%B IN (10 11 12) DO quickbms.exe -Y -f "*.dnt" quickbms.txt Resource0%%B.pak %outputdir%
 goto finish
 
 :only_script
 FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource0%%A.pak %outputdir%
-quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource10.pak %outputdir%
-quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource11.pak %outputdir%
-quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource12.pak %outputdir%
+FOR %%B IN (10 11 12) DO quickbms.exe -Y -f "*.xml;*.lua" quickbms.txt Resource%%B.pak %outputdir%
 goto finish
 
 :all_noaudiovideo
 FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource0%%A.pak %outputdir%
-quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource10.pak %outputdir%
-quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource11.pak %outputdir%
-quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource12.pak %outputdir%
+FOR %%B IN (10 11 12) DO quickbms.exe -Y -f "!*.ogg;!*.avi;!*.dds;!*.mp3;!*.wav" quickbms.txt Resource%%B.pak %outputdir%
 goto finish
 
 :all
 FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y quickbms.txt Resource0%%A.pak %outputdir%
-quickbms.exe -Y quickbms.txt Resource10.pak %outputdir%
-quickbms.exe -Y quickbms.txt Resource11.pak %outputdir%
-quickbms.exe -Y quickbms.txt Resource12.pak %outputdir%
+FOR %%B IN (10 11 12) DO quickbms.exe -Y quickbms.txt Resource%%B.pak %outputdir%
 goto finish
 
 :finish
